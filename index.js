87054e06ef70c9fe74e015b12485627095fa22c5
AniketKumarPatel <apatel08011@gmail.com>
2026-06-29 12:47:42 +0530

    this is my first commit

---
+++ index.js
@@ -0,0 +1,161 @@
+// var age = 256;
+
+// if (true) {
+//     console.log(age);
+// }
+
+// function solve(){
+//     var age = 34;
+//     console.log(age);
+// }
+// so lve()
+// age =10;
+// if(age>18){
+//     console.log('vote');
+// }
+// else{
+//     console.log('con not vote')
+// }
+
+// let number = 2;
+// if (number == 23) {
+//     console.log('A');
+// }
+// else if (number == 2) {
+//     console.log('B');
+// }
+// else {
+//     console.log('c');
+// }
+
+//loops
+
+
+
+// for(let i=1; i<10; i++){
+//     console.log('i');
+// }
+
+// for(let i=6; i>=1; i--){
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 6; i++) {
+//     if (i == 3) {
+//         break;
+
+//     }
+//     else {
+//         console.log(i);
+//     }
+// }
+
+// for(let i =1; i<=5; i++){
+//     if(i==4){
+//         continue;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+// let i = 1;
+// while (i < 5) {
+//     console.log("Hii");
+//     if (i == 3) {
+//         i++;
+//         continue;
+//     }
+//     else
+//         console.log(i);
+//     i++;
+// }
+
+
+//String
+// let op1 = 'English';
+
+// let op2 = 'Hindi';
+// console.log(op1.length);
+
+// let ans = op1 + op2;
+// console.log(ans);
+
+// let str = "Aniket and Anshu";
+// console.log(str.substring(2, 4));
+
+//Function
+
+
+// console.log("Aniket");
+
+// function Myname(){
+//     console.log("Love Aniket")
+// }
+// Myname()
+
+// function printCounting(){
+//     for(let i=-1; i<=10000000000;i++){
+//         console.log(i);
+//     }
+// }
+// printCounting()
+
+
+// function printNumber(num){
+//     console.log("Printing Number:",num);
+// }
+// printNumber(6);
+
+// function getAverage(num1,num2){
+//     let avg =(num1+num2)/2;
+//     console.log(avg);
+// }
+// getAverage(5,5);
+
+//return function
+
+// function getSum(a,b,c){
+//     let sum = a+b+c;
+//     return sum;
+// }
+//  let ans = getSum(1,2,3);
+//  console.log("Printing Sum:",ans);
+
+
+// function getName(firstNmae,lastName){
+//     let fullName = firstNmae+" " +lastName;
+//     return fullName;
+// }
+// let fullName= getName("Aniket" , "patel");
+// console.log ("Full Name:", fullName);
+
+//OBJECT
+
+// let obj ={
+//     name:"Aniket",
+//     age:23,
+//     greet:function(){
+//         console.log("hello ji kasi ho sab");
+//     }
+
+// };
+// console.log(obj)
+// obj.greet();
+// console.log(typeof(obj))
+
+//ARRAY Collection of items
+
+// let arr =[1,2,3,4,5,];
+
+
+// console.log(arr);
+
+//ARRAY CONSTRUCTOR
+
+let arr = new Array('love',1, true);
+arr.push("Anijet");
+arr.pop("anijet");
+console.log(arr);
+console .log(typeof(obe));
+
